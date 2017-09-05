2346a118f4a40a1239925f8708f35f8dd9f301c2
heojoung2 <heojoung2@gmail.com>
2017-09-05 15:20:51 +0900

    add

--- Colorization_slim/test.py
+++ Colorization_slim/test.py
@@ -6,7 +6,43 @@ import numpy as np
 import math
 import cv2
 
-def Fusion(input1,input2,name):
+def colorization_arg_scope(weight_decay=0.00004,
+                           use_batch_norm=True,
+                           batch_norm_var_collection='moving_vars'):
+    batch_norm_params = {
+        # Decay for the moving averages.
+        'decay': 0.9997,
+        #  epsilon to prevent 0s in variance.
+        'epsilon': 0.001,
+        #  collection containing update_ops.
+        'updates_collections': tf.GraphKeys.UPDATE_OPS,
+        #  collection containing the moving mean and moving variance.
+        'variables_collections': {
+            'beta': None,
+            'gamma': None,
+            'moving_mean': [batch_norm_var_collection],
+            'moving_variance': [batch_norm_var_collection],
+        }
+    }
+    if use_batch_norm:
+        normalizer_fn = slim.batch_norm
+        normalizer_params = batch_norm_params
+    else:
+        normalizer_fn = None
+        normalizer_params = {}
+     # Set weight_decay for weights in Conv and FC layers.
+    with slim.arg_scope(
+            [slim.conv2d, slim.fully_connected],
+            weights_regularizer=slim.l2_regularizer(weight_decay)):
+        with slim.arg_scope(
+                [slim.conv2d],
+                weights_initializer=slim.variance_scaling_initializer(),
+                activation_fn=tf.nn.relu,
+                normalizer_fn=normalizer_fn,
+                normalizer_params=normalizer_params) as sc:
+            return sc
+
+def Fusion(input1, input2, name):
     col = int(input1.shape[1])
     row = int(input1.shape[2])
     input2 = tf.tile(input2,[1,col*row])
@@ -28,36 +64,38 @@ class Model:
         self.X = tf.placeholder(tf.float32, [batch_size,self.height,self.width,1])
         self.X2 = tf.placeholder(tf.float32, [batch_size, 224, 224,1])
 
-        first_low_level = slim.stack(self.X, slim.conv2d, [(64, [3, 3], 2), (128, [3, 3], 1), (128, [3, 3], 2), (256, [3, 3], 1), (256,[3,3], 2), (512,[3,3],1)],scope='low_level')
+        with slim.arg_scope(colorization_arg_scope()):
+            with slim.arg_scope([slim.batch_norm], is_training=False):
+                first_low_level = slim.stack(self.X, slim.conv2d, [(64, [3, 3], 2), (128, [3, 3], 1), (128, [3, 3], 2), (256, [3, 3], 1), (256,[3,3], 2), (512,[3,3],1)],scope='low_level')
 
-        second_low_level1 = slim.conv2d(self.X2, 64, [3, 3], stride=2, scope='low_level/low_level_1',reuse=True)
-        second_low_level2 = slim.conv2d(second_low_level1, 128, [3, 3], scope='low_level/low_level_2',reuse=True)
-        second_low_level3 = slim.conv2d(second_low_level2, 128, [3, 3], stride=2, scope='low_level/low_level_3',reuse=True)
-        second_low_level4 = slim.conv2d(second_low_level3, 256, [3, 3], scope='low_level/low_level_4',reuse=True)
-        second_low_level5 = slim.conv2d(second_low_level4, 256, [3, 3], stride=2, scope='low_level/low_level_5',reuse=True)
-        second_low_level6 = slim.conv2d(second_low_level5, 512, [3, 3], scope='low_level/low_level_6', reuse=True)
+                second_low_level1 = slim.conv2d(self.X2, 64, [3, 3], stride=2, scope='low_level/low_level_1',reuse=True)
+                second_low_level2 = slim.conv2d(second_low_level1, 128, [3, 3], scope='low_level/low_level_2',reuse=True)
+                second_low_level3 = slim.conv2d(second_low_level2, 128, [3, 3],stride=2, scope='low_level/low_level_3',reuse=True)
+                second_low_level4 = slim.conv2d(second_low_level3, 256, [3, 3], scope='low_level/low_level_4',reuse=True)
+                second_low_level5 = slim.conv2d(second_low_level4, 256, [3, 3], stride=2, scope='low_level/low_level_5',reuse=True)
+                second_low_level6 = slim.conv2d(second_low_level5, 512, [3, 3], scope='low_level/low_level_6', reuse=True)
 
-        mid_level = slim.stack(first_low_level, slim.conv2d, [(512,[3,3]), (256,[3,3])],scope='mid_level')
+                mid_level = slim.stack(first_low_level, slim.conv2d, [(512,[3,3]), (256,[3,3])],scope='mid_level' )
 
-        global_level1 = slim.stack(second_low_level6, slim.conv2d, [(512, [3, 3], 2), (512, [3, 3], 1), (512, [3, 3], 2), (512, [3, 3], 1)], scope='global_level1')
-        global_level1 = tf.reshape(global_level1, [-1, 7*7*512])
-        global_level2 = slim.fully_connected(global_level1, 1024, scope='global_level2')
-        global_level3 = slim.fully_connected(global_level2, 512, scope='global_level3')
-        global_level4 = slim.fully_connected(global_level3, 256, scope='global_level4')
+                global_level1 = slim.stack(second_low_level6, slim.conv2d, [(512, [3, 3], 2), (512, [3, 3], 1), (512, [3, 3], 2), (512, [3, 3], 1)], scope='global_level1')
+                global_level1 = slim.flatten(global_level1)
+                global_level2 = slim.fully_connected(global_level1, 1024, scope='global_level2')
+                global_level3 = slim.fully_connected(global_level2, 512, scope='global_level3')
+                global_level4 = slim.fully_connected(global_level3, 256, scope='global_level4')
 
-        fusion_layer = Fusion(input1=mid_level, input2=global_level4, name="fusion")
+                fusion_layer = Fusion(input1=mid_level, input2=global_level4, name="fusion")
 
-        colorization_network1 = slim.conv2d(fusion_layer,128,[3,3],scope="colorization_network1")
-        colorization_network2 = tf.image.resize_images(colorization_network1, [math.ceil(self.height / 4), math.ceil(self.width / 4)])
-        colorization_network3 = slim.stack(colorization_network2, slim.conv2d, [(64,[3,3]), (64,[3,3])],scope='colorization_network3')
-        colorization_network4 = tf.image.resize_images(colorization_network3, [math.ceil(self.height / 2),math.ceil(self.width / 2)])
-        colorization_network5 = slim.conv2d(colorization_network4, 32, [3, 3], scope="colorization_network5")
+                colorization_network1 = slim.conv2d(fusion_layer,128,[3,3],scope="colorization_network1")
+                colorization_network2 = tf.image.resize_images(colorization_network1, [math.ceil(self.height / 4), math.ceil(self.width / 4)])
+                colorization_network3 = slim.stack(colorization_network2, slim.conv2d, [(64,[3,3]), (64,[3,3])],scope='colorization_network3')
+                colorization_network4 = tf.image.resize_images(colorization_network3, [math.ceil(self.height / 2),math.ceil(self.width / 2)])
+                colorization_network5 = slim.conv2d(colorization_network4, 32, [3, 3],scope="colorization_network5")
 
-        colorization_network6 = slim.conv2d(colorization_network5, classification_num, [3, 3], scope="colorization_network6", activation_fn=tf.nn.sigmoid)
-        self.colorization_network7 = tf.image.resize_images(colorization_network6, [self.height,self.width])
+                colorization_network6 = slim.conv2d(colorization_network5, classification_num, [3, 3], scope="colorization_network6", activation_fn=tf.nn.sigmoid)
+                self.colorization_network7 = tf.image.resize_images(colorization_network6, [self.height,self.width])
 
-        classification_level1 = slim.fully_connected(global_level3, 256, scope='classficatio_level1')
-        self.classification_level2 = slim.fully_connected(classification_level1, classification_num, scope='classficatio_level2')
+                classification_level1 = slim.fully_connected(global_level3, 256, scope='classfication_level1')
+                self.classification_level2 = slim.fully_connected(classification_level1, classification_num, scope='classfication_level2')
 
     def Predict_y_colorization(self, X, X2):
         return self.sess.run(self.colorization_network7, feed_dict={self.X: X, self.X2:X2})
@@ -67,7 +105,8 @@ class Model:
 
 batch_size = 1
 image_size=224
-image =cv2.imread('test_image.png')
+#image =cv2.imread('C:/users/heojo/Desktop/Colorization_slim/test_image.PNG')
+image =cv2.imread('C:/users/heojo/Desktop/Colorization_slim/Image_data/indoor/1.PNG')
 image_height,image_width,image_channel =  image.shape
 image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 X=[image[:,:,:1]/255]
@@ -86,9 +125,11 @@ colorization_result = model.Predict_y_colorization(X,X2)
 classification_result = model.Predict_y_classification(X,X2)
 
 L=np.reshape(image[:,:,:1],[image_height,image_width,1])
-lab = np.concatenate((L,colorization_result[0]), axis=2)
+ab = (colorization_result[0]*255).astype(np.uint8)
+lab = np.concatenate( (L, ab), axis=2)
 result = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
 
+cv2.imshow('gray',L)
 cv2.imshow('result : '+str(classification_result[0]),result)
 cv2.waitKey(0)
 
--- Colorization_slim/train.py
+++ Colorization_slim/train.py
@@ -7,6 +7,42 @@ import csv
 import math
 import cv2
 
+def colorization_arg_scope(weight_decay=0.00004,
+                           use_batch_norm=True,
+                           batch_norm_var_collection='moving_vars'):
+    batch_norm_params = {
+        # Decay for the moving averages.
+        'decay': 0.9997,
+        #  epsilon to prevent 0s in variance.
+        'epsilon': 0.001,
+        #  collection containing update_ops.
+        'updates_collections': tf.GraphKeys.UPDATE_OPS,
+        #  collection containing the moving mean and moving variance.
+        'variables_collections': {
+            'beta': None,
+            'gamma': None,
+            'moving_mean': [batch_norm_var_collection],
+            'moving_variance': [batch_norm_var_collection],
+        }
+    }
+    if use_batch_norm:
+        normalizer_fn = slim.batch_norm
+        normalizer_params = batch_norm_params
+    else:
+        normalizer_fn = None
+        normalizer_params = {}
+     # Set weight_decay for weights in Conv and FC layers.
+    with slim.arg_scope(
+            [slim.conv2d, slim.fully_connected],
+            weights_regularizer=slim.l2_regularizer(weight_decay)):
+        with slim.arg_scope(
+                [slim.conv2d],
+                weights_initializer=slim.variance_scaling_initializer(),
+                activation_fn=tf.nn.relu,
+                normalizer_fn=normalizer_fn,
+                normalizer_params=normalizer_params) as sc:
+            return sc
+
 def read_train_data():
     image_size=224
     X = []
@@ -24,7 +60,7 @@ def read_train_data():
         output_image = image[:,:,1:]
 
         X.append(input_image/255)
-        Y_colorization.append(output_image)
+        Y_colorization.append(output_image/255)
         Y_classification.append(int(data[1]))
 
     csv_file.close()
@@ -47,51 +83,55 @@ class Model:
         image_width_size = 224
         classification_num = 2 #205
         loss_parameter = 1/300
-        learning_rate = 0.001
+        initial_learning_rate = 1e-1
 
         self.X = tf.placeholder(tf.float32, [batch_size,image_height_size,image_width_size,1])
         self.Y_colorization = tf.placeholder(tf.float32, [batch_size,image_height_size,image_width_size,2])
         self.Y_classification = tf.placeholder(tf.int32, [batch_size])
         Y_classification_one_hot = tf.one_hot(self.Y_classification, classification_num)
 
-        first_low_level = slim.stack(self.X, slim.conv2d, [(64, [3, 3], 2), (128, [3, 3], 1), (128, [3, 3], 2), (256, [3, 3], 1), (256,[3,3], 2), (512,[3,3],1)],scope='low_level')
+        with slim.arg_scope(colorization_arg_scope()):
+            first_low_level = slim.stack(self.X, slim.conv2d, [(64, [3, 3], 2), (128, [3, 3], 1), (128, [3, 3], 2), (256, [3, 3], 1), (256,[3,3], 2), (512,[3,3],1)],scope='low_level')
 
-        second_low_level1 = slim.conv2d(self.X, 64, [3, 3], stride=2, scope='low_level/low_level_1',reuse=True)
-        second_low_level2 = slim.conv2d(second_low_level1, 128, [3, 3], scope='low_level/low_level_2',reuse=True)
-        second_low_level3 = slim.conv2d(second_low_level2, 128, [3, 3], stride=2, scope='low_level/low_level_3',reuse=True)
-        second_low_level4 = slim.conv2d(second_low_level3, 256, [3, 3], scope='low_level/low_level_4',reuse=True)
-        second_low_level5 = slim.conv2d(second_low_level4, 256, [3, 3], stride=2, scope='low_level/low_level_5',reuse=True)
-        second_low_level6 = slim.conv2d(second_low_level5, 512, [3, 3], scope='low_level/low_level_6', reuse=True)
+            second_low_level1 = slim.conv2d(self.X, 64, [3, 3], stride=2, scope='low_level/low_level_1',reuse=True)
+            second_low_level2 = slim.conv2d(second_low_level1, 128, [3, 3], scope='low_level/low_level_2',reuse=True)
+            second_low_level3 = slim.conv2d(second_low_level2, 128, [3, 3], stride=2, scope='low_level/low_level_3',reuse=True)
+            second_low_level4 = slim.conv2d(second_low_level3, 256, [3, 3], scope='low_level/low_level_4',reuse=True)
+            second_low_level5 = slim.conv2d(second_low_level4, 256, [3, 3], stride=2, scope='low_level/low_level_5',reuse=True)
+            second_low_level6 = slim.conv2d(second_low_level5, 512, [3, 3], scope='low_level/low_level_6', reuse=True)
 
-        mid_level = slim.stack(first_low_level, slim.conv2d, [(512,[3,3]), (256,[3,3])],scope='mid_level')
+            mid_level = slim.stack(first_low_level, slim.conv2d, [(512,[3,3]), (256,[3,3])], scope='mid_level')
 
-        global_level1 = slim.stack(second_low_level6, slim.conv2d, [(512, [3, 3], 2), (512, [3, 3], 1), (512, [3, 3], 2), (512, [3, 3], 1)], scope='global_level1')
-        global_level1 = tf.reshape(global_level1, [-1, 7*7*512])
-        global_level2 = slim.fully_connected(global_level1, 1024, scope='global_level2')
-        global_level3 = slim.fully_connected(global_level2, 512, scope='global_level3')
-        global_level4 = slim.fully_connected(global_level3, 256, scope='global_level4')
+            global_level1 = slim.stack(second_low_level6, slim.conv2d, [(512, [3, 3], 2), (512, [3, 3], 1), (512, [3, 3], 2), (512, [3, 3], 1)], scope='global_level1')
+            global_level1 = slim.flatten(global_level1)
+            global_level2 = slim.fully_connected(global_level1, 1024, scope='global_level2')
+            global_level3 = slim.fully_connected(global_level2, 512, scope='global_level3')
+            global_level4 = slim.fully_connected(global_level3, 256, scope='global_level4')
 
-        fusion_layer = Fusion(input1=mid_level, input2=global_level4, name="fusion")
+            fusion_layer = Fusion(input1=mid_level, input2=global_level4, name="fusion")
 
-        colorization_network1 = slim.conv2d(fusion_layer,128,[3,3],scope="colorization_network1")
-        colorization_network2 = tf.image.resize_images(colorization_network1, [math.ceil(image_height_size / 4), math.ceil(image_width_size / 4)])
-        colorization_network3 = slim.stack(colorization_network2, slim.conv2d, [(64,[3,3]), (64,[3,3])],scope='colorization_network3')
-        colorization_network4 = tf.image.resize_images(colorization_network3, [math.ceil(image_height_size / 2),math.ceil(image_width_size / 2)])
-        colorization_network5 = slim.conv2d(colorization_network4, 32, [3, 3], scope="colorization_network5")
+            colorization_network1 = slim.conv2d(fusion_layer,128,[3,3], scope="colorization_network1")
+            colorization_network2 = tf.image.resize_images(colorization_network1, [math.ceil(image_height_size / 4), math.ceil(image_width_size / 4)])
+            colorization_network3 = slim.stack(colorization_network2, slim.conv2d, [(64,[3,3]), (64,[3,3])],scope='colorization_network3')
+            colorization_network4 = tf.image.resize_images(colorization_network3, [math.ceil(image_height_size / 2),math.ceil(image_width_size / 2)])
+            colorization_network5 = slim.conv2d(colorization_network4, 32, [3, 3], scope="colorization_network5")
 
-        colorization_network6 = slim.conv2d(colorization_network5, classification_num, [3, 3], scope="colorization_network6", activation_fn=tf.nn.sigmoid)
-        colorization_network7 = tf.image.resize_images(colorization_network6, [image_height_size,image_width_size])
-        self.colorization_network7 = slim.conv2d_transpose(colorization_network6, 2, 3, stride=2, scope="colorization_network7")
+            colorization_network6 = slim.conv2d(colorization_network5, classification_num, [3, 3], scope="colorization_network6", activation_fn=tf.nn.sigmoid)
+
+            self.colorization_network7 = tf.image.resize_images(colorization_network6, [image_height_size,image_width_size])
+
+            classification_level1 = slim.fully_connected(global_level3, 256, scope='classfication_level1')
+            self.classification_level2 = slim.fully_connected(classification_level1, classification_num, scope='classfication_level2')
 
         colorization_loss = tf.losses.mean_squared_error(labels=self.Y_colorization, predictions=self.colorization_network7)
-
-        classification_level1 = slim.fully_connected(global_level3, 256, scope='classficatio_level1')
-        self.classification_level2 = slim.fully_connected(classification_level1, classification_num, scope='classficatio_level2')
-
         classification_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y_classification_one_hot, logits=self.classification_level2))
+        self.loss = colorization_loss + (loss_parameter * classification_loss)
 
-        self.loss = colorization_loss - (loss_parameter * classification_loss)
-        self.train = tf.train.AdadeltaOptimizer(learning_rate=learning_rate).minimize(self.loss)
+        update_ops = tf.get_collection_ref(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            global_step = tf.Variable(0, trainable=False)
+            learning_rate = tf.train.exponential_decay(initial_learning_rate, global_step,100, 0.90, staircase=True)
+            self.train = tf.train.AdadeltaOptimizer(learning_rate=learning_rate).minimize(self.loss)
 
         loss_graph = tf.summary.scalar("loss",self.loss)
         self.merged_graph = tf.summary.merge_all()
@@ -106,7 +146,7 @@ class Model:
         return self.sess.run(self.train,feed_dict={self.X: X, self.Y_colorization : Y_colorization, self.Y_classification : Y_classification})
 
 #varaible
-epochs = 1 #11
+epochs = 10000 #11
 batch_size = 2 #128
 X, Y_colorization, Y_classification  = read_train_data()
 
@@ -120,7 +160,7 @@ writer = tf.summary.FileWriter("./board",sess.graph)
 #train model
 for epoch in range(epochs):
     loss = model.Loss(X, Y_colorization, Y_classification)
-    print(loss)
+    print(epoch, loss)
     model.Train(X, Y_colorization, Y_classification)
     graph_loss = model.Loss_graph(X, Y_colorization, Y_classification)
     writer.add_summary(graph_loss,epoch)
